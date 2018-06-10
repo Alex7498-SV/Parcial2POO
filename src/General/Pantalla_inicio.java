21f1b617891a50fd7c596f97316fe2567f7ca7b8
Alex7498-SV <00068817@uca.edu.sv>
2018-06-09 19:01:03 -0600

    eliminacion de clases innecesarias e implementacion de patrones

--- src/General/Pantalla_inicio.java
+++ src/General/Pantalla_inicio.java
@@ -4,9 +4,8 @@
  * and open the template in the editor.
  */
 
-package RubenSiguenzas_World;
+package General;
 
-import java.util.Scanner;
 /**
  *
  * @author alex
--- src/RubenSiguenzas_World/RubenAlexanderS_World.java
+++ src/RubenSiguenzas_World/RubenAlexanderS_World.java
@@ -1,13 +1,32 @@
 
 package RubenSiguenzas_World;
 
+import Builder_Vehiculo.*;
+import Builder_recurso.*;
+import java.util.ArrayList;
+
 
 
 public class RubenAlexanderS_World {
 
 
     public static void main(String[] args) {
+        //BobElConstructor constructor = new BobElConstructor();
+        abstractVehiculo aV = new abstractVehiculo();
         
+        ArrayList<Vehiculo> arvehiculo = new ArrayList<Vehiculo>();
+        
+        arvehiculo.add(aV.getVehiculo(1, "Coaster"));
+        arvehiculo.add(aV.getVehiculo(3, "Carreta"));
+        arvehiculo.add(aV.getVehiculo(1, "Camion"));
+        arvehiculo.add(aV.getVehiculo(2, "Formula 1"));
+        arvehiculo.add(aV.getVehiculo(3, "Mickey"));
+        
+        arvehiculo.forEach(vehiculo->{
+            System.out.println("Datos de vehiculo");
+            System.out.println(vehiculo.getNombre());
+            System.out.println(vehiculo.getVida());
+        });
         
     }
 }
